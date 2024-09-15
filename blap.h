a09609daf5d9840309c031244ac61d63b0a63d81
Brady Bangasser <bradybbangasser@gmail.com>
2024-09-14 22:51:24 -0500

    blap initial test

--- blap.h
+++ blap.h
@@ -1,12 +1,15 @@
 #pragma once
 #include <inttypes.h>
 #include <stddef.h>
+#include <memory.h>
+#include <stdlib.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 static const uint16_t BLAP_MAX_PAYLOAD_LEN = 69;
+static const uint8_t BLAP_IDENTIFIER = 0x0C;
 
 enum BLAP_ERROR_CODES {
     BLAP_NOT_IMPL = -1,
@@ -15,24 +18,29 @@ enum BLAP_ERROR_CODES {
     BLAP_POINTER_NOT_VALID,
 };
 
-struct blap_packet {
+struct blap_packet_header {
     uint8_t blap_id : 4;
     uint8_t reserved_0 : 4;
     uint32_t reserved_1;
     uint8_t frag;
     uint16_t length;
-    uint8_t *payload;
-};
+} __attribute__((packed));
 
 
-uint8_t send_blap_packet(uint8_t *data, uint16_t length);
-uint8_t recv_blap_packet(uint8_t *buffer, uint16_t length);
+enum BLAP_ERROR_CODES send_blap_packet(uint8_t *data, uint16_t length);
+enum BLAP_ERROR_CODES recv_blap_packet(uint8_t *buffer, uint16_t length);
 
 /**
  * Client must implement these
  */
 uint8_t send_data(uint8_t *data, uint32_t len);
-uint8_t recv_data(uint8_t *buffer, uint32_t len);
+/**
+ * receive data from device
+ * @param buffer buffer for data
+ * @param len max length of data
+ * @returns length of received data
+ */
+uint32_t recv_data(uint8_t *buffer, uint32_t len);
 
 #ifdef __cplusplus
 }
