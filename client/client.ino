ab8d224aec52475f0136452548284dd0bc4adcb5
Brady Bangasser <bradybbangasser@gmail.com>
2024-09-16 08:19:56 -0500

    Working data transmission

--- client/client.ino
+++ client/client.ino
@@ -1,69 +1,48 @@
 #include <SPI.h>
-#include <nRF24L01.h>
-#include <RF24.h>
-
 #include "blap.h"
+#include "RF24.h"
 
-RF24 radio(D1, D2);
+#define CE_PIN D1
+#define CSN_PIN D2
+// instantiate an object for the nRF24L01 transceiver
+RF24 radio(CE_PIN, CSN_PIN);
 
-uint32_t recv_data(uint8_t *buffer, uint32_t len) {
-  uint32_t payloadLength = 0;
-  if (radio.available()) {
-    radio.read((void *) buffer, len);
-    Serial.printf("0x%x\n", buffer[0]);
-  }
+uint8_t address[][6] = { "1Node", "2Node" };
+char payload[256] = {0};
 
-  return payloadLength;
-}
-
-uint8_t send_data(uint8_t *data, uint32_t len) { return -1; }
-
-bool state = false;
-uint8_t payload = 0;
-const uint8_t pipe[] = { 0xCC, 0xCE, 0xCC, 0xCE, 0xCC };
-uint32_t data_len;
-
-inline void blink() {
+static inline void blink(uint32_t dur) {
   digitalWrite(LED_BUILTIN, LOW);
-  delay(150);
+  delay(dur);
   digitalWrite(LED_BUILTIN, HIGH);
 }
 
 void setup() {
-  Serial.begin(115200);
-
-  while (!Serial) blink();
-  Serial.print("\n\nHERE\n");
-
   pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, LOW);
 
-  if (!radio.begin()) {
-    Serial.print("Failed to connect to radio\n");
-    while (1) {
-      blink();
-      delay(200);
-    }
+  Serial.begin(115200);
+  while (!Serial) {
+    // some boards need to wait to ensure access to serial over USB
   }
 
-  radio.setChannel(115);
-  radio.openReadingPipe(0, pipe);
-  radio.startListening();
-  Serial.print("Init\n");
-  digitalWrite(LED_BUILTIN, HIGH);
+  // initialize the transceiver on the SPI bus
+  if (!radio.begin()) {
+    Serial.println("radio hardware is not responding!!");
+    while (1) {}  // hold in infinite loop
+  }
+
+  radio.setPALevel(RF24_PA_MAX);
+  // radio.setPayloadSize(sizeof(payload));
+  radio.enableDynamicPayloads();
+  radio.openReadingPipe(1, address[0]);  // using pipe 1
+  radio.startListening();  // put radio in RX mode
 }
 
 void loop() {
-  uint8_t buffer[256] = {0};
-  if (radio.available()) {
-    radio.read((void *) buffer, 256);
-    Serial.printf("0x%x\n", buffer[0]);
-  }
-  // data_len = recv_blap_packet(NULL, 256);
-  // if (data_len != -1) {
-  //   Serial.printf("len: %d\n", data_len);
-  //   blink();
-  // }
-
-  delay(50);
-}
+    uint8_t pipe;
+    if (radio.available(&pipe)) {
+      uint8_t bytes = radio.getDynamicPayloadSize();
+      radio.read(payload, bytes);
+      Serial.printf("0x%x\n", payload[0]);
+      blink(300);
+    }
+}  // loop
