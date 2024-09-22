cc8e4a56460b2791bb24601d059819a21a1b1bdc
Brady Bangasser <bradybbangasser@gmail.com>
2024-09-22 16:36:10 -0500

    little refactor

--- client/client.ino
+++ client/client.ino
@@ -4,8 +4,13 @@
 
 #define CE_PIN D1
 #define CSN_PIN D2
-// instantiate an object for the nRF24L01 transceiver
+
 RF24 radio(CE_PIN, CSN_PIN);
+
+uint32_t recv_data(uint8_t *buffer, uint32_t len) {
+      
+}
+// instantiate an object for the nRF24L01 transceiver
 
 uint8_t address[][6] = { "1Node", "2Node" };
 char payload[256] = {0};
@@ -22,12 +27,13 @@ void setup() {
   Serial.begin(115200);
   while (!Serial) {
     // some boards need to wait to ensure access to serial over USB
+    blink(200); delay(100);
   }
 
   // initialize the transceiver on the SPI bus
   if (!radio.begin()) {
     Serial.println("radio hardware is not responding!!");
-    while (1) {}  // hold in infinite loop
+    while (1) { blink(200); delay(50); }  // hold in infinite loop
   }
 
   radio.setPALevel(RF24_PA_MAX);
@@ -35,6 +41,7 @@ void setup() {
   radio.enableDynamicPayloads();
   radio.openReadingPipe(1, address[0]);  // using pipe 1
   radio.startListening();  // put radio in RX mode
+  digitalWrite(LED_BUILTIN, HIGH);
 }
 
 void loop() {
@@ -43,7 +50,8 @@ void loop() {
       uint8_t bytes = radio.getDynamicPayloadSize();
       radio.read(payload, bytes);
 
-      Serial.println(payload);
+      Serial.println("THIS");
+      Serial.println((int) payload[0]);
       blink(300);
     }
 }  // loop
